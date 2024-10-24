a27ceb25ec876c87ecd928d3b653b904d7322aa9
dila6327 <quliyevadilruba774@gmail.com>
2024-10-24 16:38:27 +0400

    Delete portfolio_2 directory

--- portfolio_2/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const light = document.querySelector('.light');
-    
-
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-
-    light.style.transform = `translate(${mouseX}px, ${mouseY}px) translate(-50%, -50%)`;
-});
